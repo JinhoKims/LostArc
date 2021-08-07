3206569c883423390e69e4473ea55e0dba79fba0
Jinho Kim <55690757+JinhoKims@users.noreply.github.com>
2021-08-08 05:40:57 +0900

    Revert "sdf"
    This reverts commit 8d3f05708e6dc6794e1499d14c02e3a0c467c4c7.

--- Source/LostArc/Private/LostArcUIInventorySlot.cpp
+++ Source/LostArc/Private/LostArcUIInventorySlot.cpp
@@ -8,4 +8,7 @@
 void ULostArcUIInventorySlot::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+	ItemImage = Cast<UImage>(GetWidgetFromName(TEXT("ItemImage")));
+	ItemText = Cast<UTextBlock>(GetWidgetFromName(TEXT("ItemText")));
 }
