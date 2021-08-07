0009fa79f77c275d2d8eea8d9fa228ea0647440e
Jinho Kim <55690757+JinhoKims@users.noreply.github.com>
2021-08-08 05:42:08 +0900

    d

---
+++ Source/LostArc/Public/LostArcUIInventory.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LostArcUIInventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOSTARC_API ULostArcUIInventory : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	UPROPERTY()
+	TArray<class ULostArcUIInventorySlot*> InventorySlot;
+};
