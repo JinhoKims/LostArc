30905fc6f587df1b05838b2e348aee4a5c39b27b
Jinho Kim <winda00zu@gmail.com>
2021-12-05 02:55:01 +0900

    6

---
+++ Source/LostArc/Public/Abilities/Items/Equip/LostArcItemEquip_Necklace.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Items/Equip/LostArcItemEquipBase.h"
+#include "LostArcItemEquip_Necklace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOSTARC_API ULostArcItemEquip_Necklace : public ULostArcItemEquipBase
+{
+	GENERATED_BODY()
+	
+};
