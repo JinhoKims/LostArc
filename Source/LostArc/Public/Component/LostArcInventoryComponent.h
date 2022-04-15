d2f0a2b49696497314800d5cf4c58b524a9f911a
Jinho Kim <winda00zu@gmail.com>
2022-04-15 21:03:34 +0900

    1

--- Source/LostArc/Public/Component/LostArcInventoryComponent.h
+++ Source/LostArc/Public/Component/LostArcInventoryComponent.h
@@ -22,6 +22,8 @@ public:
 	virtual void SwappingSlot(int32 OwnerIndex, int32 DistIndex, UActorComponent* OwnerComponent = nullptr) override;
 	virtual bool SetAbility(ULostArcAbilityBase* OwnerAbility, int32 SlotIndex = -1) override;
 	virtual ULostArcAbilityBase* GetAbility(int32 SlotIndex, bool bTrans = false) override;
+
+	UFUNCTION(BlueprintCallable)
 	bool InventorySlotEmptyCheck();
 	void DeleteItemAfterCheckingQuantity();
 
